295e85d43b189da97dd32a528fe54908800d9cc0
Igor Pustylnik <darthveider36@gmail.com>
2024-09-21 21:28:55 +0300

    Initial commit

---
+++ src/main/java/ru/vsu/cs/pustylnik_i_v/surveys/SurveysApplication.java
@@ -0,0 +1,9 @@
+package ru.vsu.cs.pustylnik_i_v.surveys;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class SurveysApplication extends Application {
+
+}
