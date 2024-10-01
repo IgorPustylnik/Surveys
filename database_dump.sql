3bc5fff6c2537c440353fc77fa9897dd09159756
igorpustylnik <darthveider36@gmail.com>
2024-10-01 23:59:52 +0300

    Add database dump

---
+++ database_dump.sql
@@ -0,0 +1,68 @@
+CREATE TYPE question_type as ENUM('single', 'multiple');
+
+CREATE TABLE categories
+(
+    id serial PRIMARY KEY,
+    name VARCHAR(255) NOT NULL UNIQUE
+);
+
+CREATE TABLE surveys
+(
+    id SERIAL PRIMARY KEY,
+    category_id INT,
+    FOREIGN KEY (category_id) REFERENCES categories(id),
+    name VARCHAR(255) NOT NULL,
+    description VARCHAR(1000),
+    created_at timestamp NOT NULL
+);
+
+CREATE TABLE questions
+(
+    id SERIAL PRIMARY KEY,
+    survey_id INT NOT NULL,
+    FOREIGN KEY (survey_id) REFERENCES surveys(id),
+    text VARCHAR(1000) NOT NULL,
+    type question_type NOT NULL
+);
+
+CREATE TABLE options
+(
+    id serial PRIMARY KEY,
+    question_id INT NOT NULL,
+    FOREIGN KEY (question_id) REFERENCES questions(id),
+    description VARCHAR(1000) NOT NULL
+);
+
+CREATE TABLE users
+(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE admins
+(
+    user_id SERIAL PRIMARY KEY,
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    email VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE sessions
+(
+    id SERIAL PRIMARY KEY,
+    survey_id INT NOT NULL,
+    FOREIGN KEY (survey_id) REFERENCES surveys(id),
+    user_id INT,
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    started_at timestamp NOT NULL,
+    finished_at timestamp NOT NULL
+);
+
+CREATE TABLE answers
+(
+    session_id INT NOT NULL,
+    FOREIGN KEY (session_id) REFERENCES sessions(id),
+    option_id INT NOT NULL,
+    FOREIGN KEY (option_id) REFERENCES options(id),
+    CONSTRAINT answers_pk PRIMARY KEY (session_id, option_id)
+);
