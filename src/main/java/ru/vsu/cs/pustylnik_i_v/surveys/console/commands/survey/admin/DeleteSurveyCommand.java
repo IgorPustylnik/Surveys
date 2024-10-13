26585f9362a8006c0f1b2c7619af85317624d386
igorpustylnik <darthveider36@gmail.com>
2024-10-13 17:29:18 +0300

    Fix commands execution

--- src/main/java/ru/vsu/cs/pustylnik_i_v/surveys/console/commands/survey/admin/DeleteSurveyCommand.java
+++ src/main/java/ru/vsu/cs/pustylnik_i_v/surveys/console/commands/survey/admin/DeleteSurveyCommand.java
@@ -2,6 +2,8 @@ package ru.vsu.cs.pustylnik_i_v.surveys.console.commands.survey.admin;
 
 import ru.vsu.cs.pustylnik_i_v.surveys.console.ConsoleAppContext;
 import ru.vsu.cs.pustylnik_i_v.surveys.console.commands.foundation.AppCommand;
+import ru.vsu.cs.pustylnik_i_v.surveys.console.commands.foundation.CommandType;
+import ru.vsu.cs.pustylnik_i_v.surveys.console.util.ConsoleUtils;
 
 public class DeleteSurveyCommand extends AppCommand {
 
@@ -16,6 +18,16 @@ public class DeleteSurveyCommand extends AppCommand {
 
     @Override
     public void execute() {
+        Boolean confirmation = ConsoleUtils.confirm("delete this survey");
 
+        ConsoleUtils.clear();
+        if (confirmation != null && confirmation) {
+            appContext.getSurveysService().deleteSurvey(appContext.currentSurvey.getId());
+            appContext.currentSurvey = null;
+            appContext.getCommandExecutor().getCommand(CommandType.LIST_SURVEYS).execute();
+        } else {
+            appContext.getCommandExecutor().getCommand(CommandType.UNKNOWN).execute();
+            appContext.getCommandExecutor().getCommand(CommandType.OPEN_SURVEY).execute();
+        }
     }
 }
--- src/main/java/ru/vsu/cs/pustylnik_i_v/surveys/console/commands/survey/admin/edit/EditSurveyCommand.java
+++ src/main/java/ru/vsu/cs/pustylnik_i_v/surveys/console/commands/survey/admin/edit/EditSurveyCommand.java
@@ -2,6 +2,7 @@ package ru.vsu.cs.pustylnik_i_v.surveys.console.commands.survey.admin.edit;
 
 import ru.vsu.cs.pustylnik_i_v.surveys.console.ConsoleAppContext;
 import ru.vsu.cs.pustylnik_i_v.surveys.console.commands.foundation.CommandMenu;
+import ru.vsu.cs.pustylnik_i_v.surveys.console.commands.foundation.CommandType;
 
 import java.util.ArrayList;
 
@@ -13,5 +14,11 @@ public class EditSurveyCommand extends CommandMenu {
     @Override
     public String getName() {
         return "Edit survey";
+    }
+
+    @Override
+    public void execute() {
+        System.err.println("Cannot edit a survey yet");
+        appContext.getCommandExecutor().getCommand(CommandType.OPEN_SURVEY).execute();
     }
 }
